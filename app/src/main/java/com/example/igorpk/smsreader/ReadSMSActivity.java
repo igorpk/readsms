2bf433185baa8c547830945f6c9da776546e4c16
Igor <igorpk@localhost.localdomain>
2015-10-15 02:44:25 +0200

    0.02 Commented, cleaned and working. Returns an array.tostring of fnb amounts. Does not understand debit/credit yet and treats both the same. Has a pesky <null> print bug.

--- app/src/main/java/com/example/igorpk/smsreader/ReadSMSActivity.java
+++ app/src/main/java/com/example/igorpk/smsreader/ReadSMSActivity.java
@@ -5,54 +5,55 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Read SMS Messages on the Android device
+ *
+ *         Column ID    -   Column Name
+
+ 0                      :      _id
+ 1                      :     thread_id
+ 2                      :     address
+ 3                      :     person
+ 4                      :     date
+ 5                      :     protocol
+ 6                      :     read
+ 7                      :    status
+ 8                      :    type
+ 9                      :    reply_path_present
+ 10                   :    subject
+ 11                   :    body
+ 12                   :    service_center
+ 13                   :    locked
+
+ @TODO: Consider filtering messages based on MSISDN. Check Number -> If in FNB whitelist (+27820070***), continue -> Else skip.
+ */
 public class ReadSMSActivity extends Activity {
 
-    //  GUI Widget
-    TextView lvMsg;
+    TextView textOutput;
 
-    String smsAggregate;
+    /* @TODO: Figure out how to change this to a float. */
+    ArrayList<String> smsAggregate;
 
-    // Cursor Adapter
-    SimpleCursorAdapter adapter;
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_readsms);
 
-        lvMsg = (TextView) findViewById(R.id.lvMsg);
+        textOutput = (TextView) findViewById(R.id.lvMsg);
+
+        smsAggregate = new ArrayList<String>();
 
         // Define the source uri
         Uri inboxURI = Uri.parse("content://sms/inbox");
 
-        // List required columns
-//        Column ID    -   Column Name
-//
-//        0                      :      _id
-//        1                      :     thread_id
-//        2                      :     address
-//        3                      :     person
-//        4                      :     date
-//        5                      :     protocol
-//        6                      :     read
-//        7                      :    status
-//        8                      :    type
-//        9                      :    reply_path_present
-//        10                   :    subject
-//        11                   :    body
-//        12                   :    service_center
-//        13                   :    locked
+        // Query columns
+        /* @TODO: Review the fields needed - maybe there is an easier way? */
         String[] dbColumns = new String[] { "_id", "address", "body", "date" };
 
         // Get Content Resolver object, which will deal with Content Provider
@@ -61,33 +62,35 @@ public class ReadSMSActivity extends Activity {
         // Fetch Inbox SMS Message from Built-in Content Provider
         Cursor c = cr.query(inboxURI, dbColumns, null, null, null);
 
+        /* @TODO: Convert this to 1 month - function to convert seconds. */
         // Set current timestamp for comparison later
         long back24Hours = (System.currentTimeMillis() - 84600000);
 
         // Iterate over cursor to populate a string of messages
         while(c.moveToNext()) {
+            /* @TODO: GET RID OF THE FUCKING null IN THE RESULTS!. */
 
             long smsDate = Long.parseLong(c.getString(3));
-
-            if(smsDate > (back24Hours) ) {
+            float total = 0;
+            if (smsDate > (back24Hours)) {
                 // We only want to retrieve SMS messages from FNB at this point
-                Pattern mPattern = Pattern.compile("^FNB\\s\\W{3}\\sR?.(\\d*\\.\\d*)");
+                /* @TODO:   Expand on this regex - it works, but it's too broad. The goal is to
+                   @TODO:   isolate the monetary portion of the sms only when from FNB */
+                // \\d*     any amount of digits
+                // \\.      followed by one period
+                // \\d{2}   followed by two digits
+                Pattern contentPattern = Pattern.compile("\\d*\\.\\d{2}");
 
-                Matcher matcher = mPattern.matcher(c.getString(2));
-                if(matcher.find())
-                {
-                    smsAggregate += matcher.group(0) +  "\n";
-                    //smsAggregate += matcher.toString() +  "\n";
-                    //smsAggregate += smsDate + " " + back24Hours +  "\n";
+                Matcher contentMatcher = contentPattern.matcher(c.getString(2));
+
+                /* @TODO: change find() to match()?  */
+                if (contentMatcher.find()) {
+                        String mew = contentMatcher.group(0);
+                        smsAggregate.add(mew);
                 }
             }
         }
-
-        lvMsg.setText(smsAggregate);
-//        Attached Cursor with adapter and display in listview
-//        adapter = new SimpleCursorAdapter(this, R.layout.row, c,
-//                new String[] { "body", "address" }, new int[] {
-//                R.id.lblMsg, R.id.lblNumber });
-//        lvMsg.setAdapter(adapter);
+        /* @TODO POST a json payload to a host too?*/
+        textOutput.setText(String.valueOf(smsAggregate));
     }
 }
